8b0c6f30dba7f1810924e2595cbb4ba5bb4cfecb
jamierose <jrose111284@gmail.com>
2015-11-24 19:48:08 +0000

    first half of chapter

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -9,14 +9,24 @@ feature 'restaurants' do
     end
   end
 end
-  context 'restaurant have been added' do
-    before do
-      Restaurant.create(name: KFC)
-    end
+context 'restaurants have been added' do
+  before do
+    Restaurant.create(name: 'KFC')
+  end
 
-    scenario 'display restaurant' do
-      visit '/restaurant'
-      expect(page).to have_content('KFC')
-      expect(page).not_to have_content('No restaurant yet')
+  scenario 'display restaurants' do
+    visit '/restaurants'
+    expect(page).to have_content('KFC')
+    expect(page).not_to have_content('No restaurants yet')
+  end
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
     end
+  end
 end
