dbb9d52de5860913c739924ee77cadf35d9aa77c
Norm Fasey <norm@fasey.net>
2015-11-25 17:51:40 +0000

    Nearly done

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :restaurants
-
+  root to: 'restaurants#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
   resources :restaurants do
-   resources :reviews
+    resources :reviews
   end
- root to: 'restaurants#index'
 end
